ab39730896538edec7ca0a75d918ed4f379b335b
Edleron <Edleron@hotmail.com>
2025-03-21 17:17:53 +0300

    add : initial project setup for SOLID principles demonstration

---
+++ SOLID/Description/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, OOP & SOLID !");
+
+/* # OOP Amacı # */
+/* Sürdürülebilirlik */
+/* Genişletilebilirlik */
+/* Tekrar Kullanılabilirlik */
+/* Okunabilirlik */
+
+/* # OOP İlkeleri # */
+/* 1. Encapsulation (Kapsülleme) */
+/* 2. Inheritance (Kalıtım) */
+/* 3. Polymorphism (Çok Biçimlilik) */
+/* 4. Abstraction (Soyutlama) */
+
+/* Advanced OOP Felsefesi yada Solid ilkeleri */
+/* 1. Single Responsibility Principle (SRP) (Tek Sorumluluk İlkesi)*/
+/* 2. Open Closed Principle (OCP) (Açık Kaplı Prensip) */
+/* 3. Liskov Substitution Principle (LSP) (Liskov'un Yer Değiştirme Prensibi) */
+/* 4. Interface Segregation Principle (ISP) (Arayüz Ayrımı Prensibi) */
+/* 5. Dependency Inversion Principle (DIP) (Bağımlıkların Ters ÇevrilmesiPrensibi) */
+
+/* Using Solid */
+/* 1. Daha Düzenli Kodlar */
+/* 2. Daha Az Hata */
+/* 3. Daha Az Bağımlılık */
+/* 4. Daha Kolay Bakım */
+/* 5. Daha da Kuralcı Kodlar */
+
+/* # Avantajlar # */
+// Daha Düzenli Kodlar
+// Projemizi avantaj getirici bir yere taşır.
+// Projenin organizyonunu sağlar.
+// Ekip çalışmasında.
+// Gerek projenin başka bir yere taşınmasında
+// Kodun temizliği ve yönetilibirlili açısından önemlidir.
+
+/* Single Responsibility Principle (SRP) (Tek Sorumluluk İlkesi) */
+// Bu prensip, Bir sınınıfın veya modülün sadece bir sorumluluğu olmasını belirtir. Yani, bir sınıfın yalnızca tek bir iş veya görevi olmalı ve bu göreve dair tüm işlevri yerine getirmelidir.
+
+/* Open Closed Principle (OCP) (Açık Kaplı Prensip) */
+// Yazılım bileşenleri (Sınfılar, modüller vb.) genişletilmeye açık, ancak değiştirilmeye kapalı olmalıdır. Yani var olan bir sınıfın davranışı değiştirilmeden yeni işlevsellikler eklenebilir olmalıdır.
+
+/* Liskov Substitution Principle (LSP) (Liskov'un Yer Değiştirme Prensibi) */
+// Türemiş sınfılar, temel sınfıların tüm özelliklerini ve işlevlerini değiştirmeden kullanılması ve temel sınıfın yerini alabilmelidir. Bu bir alt sınıfın üst sınıfın tüm davranışları doğru bir şekilde uygulaması gerektiği anlamına gelir.
+
+/* Interface Segregation Principle (ISP) (Arayüz Ayrımı Prensibi) */
+// Bir sınıf, kullanmadığı metodları içeren geniş kapsamlı bir arayüze(interface) bağlı olmamalıdır. Bunun yereine, spefik görevleri yerine getiren daha küçük ve odaklanmış arayüzlere sahip olmalıdır.
+
+/* Dependency Inversion Principle (DIP) (Bağımlıkların Ters Çevrilmesi Prensibi) */
+// Türemiş sınıflar, temel sınfıların tüm özelliklerini ve işlevlerini değiştirmeden kullanılması ve temel sınıfın yerini alabilmelidir. Bu bir alt sınıfın üst sınıfın tüm davranışları doğru bir şekilde uygulaması gerektiği anlamına gelir.
